c94682764583277f5afa5ddfcc9c0f8280a582f2
JanoahDev <jonah.de.smet@student.howest.be>
2020-06-21 01:22:30 +0200

    Basis van de Dashboard home uitwerkt in Css/html #3

---
+++ src/containers/Dashboard/Content/Home/CardTriplog/CardTriplog.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { ROUTES } from "../../consts";
+
+import style from "./CardTriplog.module.css";
+
+const CardTriplog = () => {
+    return (
+        <p></p>
+    );
+};
+
+export default CardTriplog;
