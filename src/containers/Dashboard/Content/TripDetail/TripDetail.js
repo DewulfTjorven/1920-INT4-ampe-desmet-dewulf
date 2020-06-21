a994c3a116e52874588abdd6abd09faf412a02f5
JanoahDev <jonah.de.smet@student.howest.be>
2020-06-21 16:32:49 +0200

    Trip ophalen op id op Detailpagina Closes #17

--- src/containers/Dashboard/Content/TripDetail/TripDetail.js
+++ src/containers/Dashboard/Content/TripDetail/TripDetail.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { useParams } from "react-router-dom";
 import { useHistory } from "react-router-dom";
+import { useStores } from "../../../../hooks/useStores";
 
 import { ROUTES } from "../../../../consts";
 
@@ -11,19 +12,22 @@ import LooseButton from "../../../../components/LooseButton/LooseButton";
 
 
 const TripDetail = () => {
-
+    // History en Store inladen
     const history = useHistory();
+    const { tripStore } = useStores();
 
+    //ID ophalen uit url
     const { id } = useParams();
 
+    //Trip ophalen op ID
     console.log(id);
+    const trip = tripStore.getTripById(id);
 
 
-
+    // Back button
     const handleBackButton = e => {
         e.preventDefault();
         history.push(ROUTES.dashboardTrips);
-        console.log(`jeepse`);
     };
 
     return (
@@ -31,7 +35,7 @@ const TripDetail = () => {
             <div className={style.container}>
                 <section className={style.header}>
                     <LooseButton onClick={handleBackButton} cta="Go back" />
-                    <h1 className={style.tripName}>Mijn eerste reis</h1>
+                    <h1 className={style.tripName}>{trip.name}</h1>
                 </section>
             </div>
         </>
