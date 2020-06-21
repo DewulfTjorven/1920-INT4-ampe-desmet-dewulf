b676c74f3265399309e625f2c1a859642ad403db
DewulfTjorven <tjorven.dewulf@student.howest.be>
2020-06-22 00:46:15 +0200

    added register and pilot button

--- src/containers/Register/RegisterContainer/Form/RegisterForm/RegisterForm.js
+++ src/containers/Register/RegisterContainer/Form/RegisterForm/RegisterForm.js
@@ -8,14 +8,16 @@ const RegisterForm = () => {
     <div className={style.login}>
         <h1 className={style.title}>Register</h1>
         <div className={style.line}></div>
-        <label className={style.label}>Full name</label>
+        <label className={style.label}>First name</label>
+        <input className={style.input} type="email"></input>
+        <label className={style.label}>Last name</label>
         <input className={style.input} type="email"></input>
         <label className={style.label}>E-mail</label>
         <input className={style.input} type="email"></input>
         <label className={style.label}>Password</label>
         <input  className={style.input}type="password"></input>
-        <input className={style.check} type="checkbox" placeholder="check me"></input> Remember me
-        <button className={style.button} type= "submit">Sign in</button>
+        <input className={style.check} type="checkbox" placeholder="check me"></input>I Agree to the terms of service and privacy policy
+        <button className={style.button} type= "submit">Sign up</button>
     </div>
   );
 };
--- src/containers/Register/RegisterContainer/RegisterContainer.js
+++ src/containers/Register/RegisterContainer/RegisterContainer.js
@@ -3,11 +3,20 @@ import React from "react";
 import Form from "./Form/Form";
 
 import style from "./RegisterContainer.module.css";
+import signin from "../../../img/pilot.png";
+
 
 const RegisterContainer = () => {
   return (
     <div className={style.container}>
       <Form />
+      <div className={style.pilot__button}>
+        <img src={signin} width="60px" height="60px" alt="lock"></img>
+        <div>
+          <h1 className={style.pilot__title}>I'm a pilot</h1>
+          <div className={style.pilot__line}></div>
+        </div>
+      </div>
     </div>
   );
 };
