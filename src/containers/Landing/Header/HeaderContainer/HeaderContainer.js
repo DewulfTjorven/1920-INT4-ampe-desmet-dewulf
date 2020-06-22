e552dee336f39618e6ac593af2663be5ec52d8ce
DewulfTjorven <tjorven.dewulf@student.howest.be>
2020-06-22 15:08:15 +0200

    added title interaction

--- src/containers/Landing/Header/HeaderContainer/HeaderContainer.js
+++ src/containers/Landing/Header/HeaderContainer/HeaderContainer.js
@@ -1,4 +1,4 @@
-import React, { } from 'react';
+import React, {useEffect, useState } from 'react';
 
 // import { ROUTES } from "../../consts";
 import Title from "../../../../components/Title/Title";
@@ -9,19 +9,23 @@ import hero from "../../../../img/hero.png";
 
 
 const HeaderContainer = () => {
+
+  let [count, setCount] = useState(0);
+  let titles = ["title 1" , "title 2", "title 3"];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count++);
+    }, 7000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-
-    //useEffect(() => {
-    //  const timer = setTimeout(() => {
-    //  }, 7000);
-    //  return () => clearTimeout(timer);
-    //}, []),
-
     <div className={style.container}>
       <Title
-        title="EXPLORE THE WORLD WITH FAMILY & FRIENDS"
+        title={titles[count]}
         text="Discover the world through a live bird’s eye view.
-        View your favourite spots or discover new ones online, with your friends, family or other someone you don’t even know." />
+        View your favourite spots or discover new ones online, with your friends, famsily or other someone you don’t even know." />
       <div className={style.hero}>
         <img className={style.hero__image} src={hero} height="728px" width="874px" alt="Island with people and drone view"></img>
       </div>
