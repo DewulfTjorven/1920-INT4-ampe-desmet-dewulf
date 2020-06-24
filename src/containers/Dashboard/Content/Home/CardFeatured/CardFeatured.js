3b8934fd4776ca583911dba27b4e2810f797717e
JanoahDev <jonah.de.smet@student.howest.be>
2020-06-24 11:41:01 +0200

    Featured locations homepage toegevoegd Closes #58

--- src/containers/Dashboard/Content/Home/CardFeatured/CardFeatured.js
+++ src/containers/Dashboard/Content/Home/CardFeatured/CardFeatured.js
@@ -1,12 +1,25 @@
 import React from "react";
-// import { ROUTES } from "../../consts";
+import { ROUTES } from "../../../../../consts";
 
 import style from "./CardFeatured.module.css";
 
+
+import FeaturedLocation from "../../../../../components/FeaturedLocation/FeaturedLocation"
+import { useStores } from "../../../../../hooks/useStores";
+
 const CardFeatured = () => {
+
+    const { locationStore } = useStores();
+    console.log(locationStore.locations);
+
     return (
         <section className={style.dashboard_featured}>
             <h2 className={style.subtitle}>Featured</h2>
+            <div className={style.locationContainers}>
+                {locationStore.locations.map(location => (
+                    <FeaturedLocation key={location.id} location={location} />
+                ))}
+            </div>
         </section>
     );
 };
